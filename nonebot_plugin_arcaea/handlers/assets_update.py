703ec92e6254051c7bb40a4af347b4f99d75295e
Lyn DK <d1366160279@gmail.com>
2022-03-17 23:41:06 +0800

    Update assets_update.py

--- nonebot_plugin_arcaea/handlers/assets_update.py
+++ nonebot_plugin_arcaea/handlers/assets_update.py
@@ -3,16 +3,17 @@ from nonebot.params import CommandArg
 from nonebot.log import logger
 from nonebot.exception import ActionFailed
 from ..matcher import arc
-from ..assets_updater import check_update
+from ..assets_updater import check_song_update, check_char_update
 
 
-async def assets_update_handler(bot: Bot, event: MessageEvent, args: Message=CommandArg()):
-    args:list = str(args).split()
+async def assets_update_handler(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    args: list = str(args).split()
     if args[0] == "assets_update":
-        result = await check_update()
+        result_song = await check_song_update()
+        result_char = await check_char_update()
         try:
             await arc.finish("\n".join([f"> {event.sender.card or event.sender.nickname}",
-            f"成功更新 {len(result)} 张曲绘:",
-            ", ".join(result)]))
+            f"成功更新 {len(result_song)} 张曲绘:",
+            f"成功更新 {len(result_char)} 张立绘"]))
         except ActionFailed as e:
             logger.error(f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}')
