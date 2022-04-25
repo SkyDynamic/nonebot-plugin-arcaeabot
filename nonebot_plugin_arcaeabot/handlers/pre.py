b3ef94174860dd756ecada7fbbd3300c628ed55d
SEAFHMC <d1366160279@gmail.com>
2022-04-25 10:05:25 +0800

    更好的错误提示

--- nonebot_plugin_arcaeabot/handlers/pre.py
+++ nonebot_plugin_arcaeabot/handlers/pre.py
@@ -16,3 +16,16 @@ async def pre_handler(bot: Bot, event: MessageEvent, args: Message = CommandArg(
         "best",
     ]:
         await arc.finish("不支持的命令参数")
+    else:
+        await arc.finish(
+            "\n".join(
+                [
+                    "/arc bind {arcaea id} 进行绑定。",
+                    "/arc unbind 解除绑定。",
+                    "/arc info 查看绑定信息。",
+                    "/arc recent 查询上一次游玩记录。",
+                    "/arc b30 查询 best 30 记录。",
+                    "/arc assets_update 更新曲绘与立绘资源",
+                ]
+            )
+        )
