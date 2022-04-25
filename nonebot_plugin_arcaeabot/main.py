ffeb5cc109af372d60be165f7e4fde7d10962260
SEAFHMC <d1366160279@gmail.com>
2022-04-25 10:11:29 +0800

    更好的错误提示

--- nonebot_plugin_arcaeabot/main.py
+++ nonebot_plugin_arcaeabot/main.py
@@ -1,8 +1,8 @@
 from .matcher import arc
 from .handlers import (
+    pre_handler,
     help_handler,
     bind_handler,
-    pre_handler,
     assets_update_handler,
     unbind_handler,
     info_handler,
@@ -12,9 +12,9 @@ from .handlers import (
 )
 
 
+arc.handle()(pre_handler)
 arc.handle()(help_handler)
 arc.handle()(bind_handler)
-arc.handle()(pre_handler)
 arc.handle()(assets_update_handler)
 arc.handle()(unbind_handler)
 arc.handle()(info_handler)
