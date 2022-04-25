f0494548ec227980000d5e8cb96d8ed5ec34f28d
SEAFHMC <d1366160279@gmail.com>
2022-04-25 09:58:10 +0800

    test

--- nonebot_plugin_arcaeabot/handlers/best.py
+++ nonebot_plugin_arcaeabot/handlers/best.py
@@ -12,67 +12,55 @@ from ..database import alias
 async def best_handler(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     args: list = str(args).split()
     if args[0] == "best":
-        user_info = UserInfo.get_or_none(UserInfo.user_qq == event.user_id)
-        song_alias = (
-            alias.get_or_none(alias.alias == args[1].strip())
-            if alias.get_or_none(alias.alias == args[1].strip())
-            else alias.get_or_none(alias.sid == args[1].strip())
-        )
-        song_id = song_alias.sid if song_alias else None
-        difficulty = args[2].strip()
-        # Exception
-        if not user_info:
-            try:
-                await arc.finish(
-                    "\n".join(
-                        [f"> {event.sender.card or event.sender.nickname}", "你还没绑定哦~"]
-                    )
-                )
-            except ActionFailed as e:
-                logger.exception(
-                    f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
-                )
-                return
-
-        if not song_id:
-            try:
-                await arc.finish(
-                    "\n".join(
-                        [
-                            f"> {event.sender.card or event.sender.nickname}",
-                            "没有找到歌曲信息，请检查曲名是否有误",
-                        ]
-                    )
-                )
-            except ActionFailed as e:
-                logger.exception(
-                    f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
-                )
-                return
-        # Query
-        if not UserArcaeaInfo.is_querying(user_info.arcaea_id):
-            result = await UserArcaeaInfo.draw_best(
-                arcaea_id=user_info.arcaea_id, song_id=song_id, difficulty=difficulty
+        if len(args) == 3:
+            user_info = UserInfo.get_or_none(UserInfo.user_qq == event.user_id)
+            song_alias = (
+                alias.get_or_none(alias.alias == args[1].strip())
+                if alias.get_or_none(alias.alias == args[1].strip())
+                else alias.get_or_none(alias.sid == args[1].strip())
             )
-            try:
-                await arc.finish(MessageSegment.reply(event.message_id) + result)
-            except ActionFailed as e:
-                logger.exception(
-                    f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
-                )
-                return
-        else:
-            try:
-                await arc.finish(
-                    "\n".join(
-                        [
-                            f"> {event.sender.card or event.sender.nickname}",
-                            "您已在查询队列, 请勿重复发起查询。",
-                        ]
+            song_id = song_alias.sid if song_alias else args[1].strip()
+            difficulty = args[2].strip()
+            # Exception
+            if not user_info:
+                try:
+                    await arc.finish(
+                        "\n".join(
+                            [f"> {event.sender.card or event.sender.nickname}", "你还没绑定哦~"]
+                        )
                     )
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+
+            # Query
+            if not UserArcaeaInfo.is_querying(user_info.arcaea_id):
+                result = await UserArcaeaInfo.draw_best(
+                    arcaea_id=user_info.arcaea_id, song_id=song_id, difficulty=difficulty
                 )
-            except ActionFailed as e:
-                logger.exception(
-                    f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
-                )
-                return
+                try:
+                    await arc.finish(MessageSegment.reply(event.message_id) + result)
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+            else:
+                try:
+                    await arc.finish(
+                        "\n".join(
+                            [
+                                f"> {event.sender.card or event.sender.nickname}",
+                                "您已在查询队列, 请勿重复发起查询。",
+                            ]
+                        )
+                    )
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+        else:
+            arc.finish("参数输入有误！")
