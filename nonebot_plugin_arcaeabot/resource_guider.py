b66386e98f1f6206485f984d0e9a0cdac3780d89
SEAFHMC <d1366160279@gmail.com>
2022-06-13 22:16:09 +0800

    摸了

--- nonebot_plugin_arcaeabot/resource_guider.py
+++ nonebot_plugin_arcaeabot/resource_guider.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-root = Path().absolute() / "resource"
+root = Path(__file__).parent.absolute() / "resource"
 root.mkdir(exist_ok=True, parents=True)
 
 
 class StaticPath:
     b30_bg = root / "b30" / "B30.png"
 
+
 from PIL import Image
+
 print(StaticPath.b30_bg)
 res = Image.open(StaticPath.b30_bg).show()
