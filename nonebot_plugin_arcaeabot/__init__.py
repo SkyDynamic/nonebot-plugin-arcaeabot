f65faecdd975387f56203960b37b37418fb3cec1
SEAFHMC <d1366160279@gmail.com>
2023-05-13 13:00:05 +0800

    version 3.1.15

--- nonebot_plugin_arcaeabot/__init__.py
+++ nonebot_plugin_arcaeabot/__init__.py
@@ -28,7 +28,7 @@ __plugin_meta__ = PluginMetadata(
         "author": [
             "SEAFHMC <soku_ritsuki@outlook.com>" "SkyDynamic <SkyDynamic@outlook.com>"
         ],
-        "version": "3.1.14",
+        "version": "3.1.15",
     },
 )
 
